e62868344f910de5b8314b33ce3d3f994c7d6928
khaled khattab <91126212+Khattab222@users.noreply.github.com>
2023-08-20 00:57:09 +0300

    socket working

--- index.js
+++ index.js
@@ -39,7 +39,8 @@ const io = new Server(server,{
 io.on('connection',socket =>{
 // console.log("connected to socket io");
 socket.on('setup',(userData) => {
-   
+   socket.join(userData?._id)
+
     socket.emit('connected');
   
 })
